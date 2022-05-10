66f1c1535174f90ccf8be2ddeb3b419c84a90cfb
unknown <hairealmta@gmail.com>
2022-05-11 06:56:04 +0700

    4

---
+++ src/com/company/Year.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import com.sun.imageio.plugins.jpeg.JPEGStreamMetadataFormat;
+
+import java.util.Scanner;
+
+public class Year {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+        boolean isLeapYear = false;
+
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if(isDivisibleBy4){
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if(isDivisibleBy100){
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if(isDivisibleBy400){
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if(isLeapYear){
+            System.out.printf("%d Là năm nhuận", year);
+        } else {
+            System.out.printf("%d không phải năm nhuận", year);
+        }
+    }
+}
